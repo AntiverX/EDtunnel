df99703362982ba5265eed63cee3b5a0a63126dc
Shuaipeng Wang <Antiver@foxmail.com>
2024-05-05 22:05:30 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -5,8 +5,8 @@ import { connect } from 'cloudflare:sockets';
 // [Windows] Press "Win + R", input cmd and run:  Powershell -NoExit -Command "[guid]::NewGuid()"
 let userID = 'd342d11e-d424-4583-b36e-524ab1f0afa4';
 // let proxyIP = '8.222.193.65';
-// const พร็อกซีไอพีs = ['8.219.209.14'];
-// let พร็อกซีไอพี = พร็อกซีไอพีs[Math.floor(Math.random() * พร็อกซีไอพีs.length)];
+const พร็อกซีไอพีs = ['8.222.193.65'];
+let พร็อกซีไอพี = พร็อกซีไอพีs[Math.floor(Math.random() * พร็อกซีไอพีs.length)];
 
 
 let dohURL = 'https://sky.rethinkdns.com/1:-Pf_____9_8A_AMAIgE8kMABVDDmKOHTAKg='; // https://cloudflare-dns.com/dns-query or https://dns.google/dns-query
